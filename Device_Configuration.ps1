2158ffebc741442c825da3c7df70894c64a56cca
Pavel Mirochnitchenko <43411053+yannara@users.noreply.github.com>
2022-11-23 13:20:59 +0200

    Add files via upload

---
+++ Device_Configuration.ps1
@@ -0,0 +1,3 @@
+﻿#Disable Lockscreen to blur
+New-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" -ErrorAction SilentlyContinue
+New-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" -Name DisableAcrylicBackgroundOnLogon -Value "1" -PropertyType DWord -Force -ErrorAction SilentlyContinue
