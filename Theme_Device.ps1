f8f9ed434c5d8e5292a980f12a0067ca2f899eb1
Pavel Mirochnitchenko <43411053+yannara@users.noreply.github.com>
2023-01-31 10:29:29 +0200

    Add files via upload

---
+++ Theme_Device.ps1
@@ -0,0 +1,11 @@
+﻿#Create new folder for Theme pictures if not exist
+New-Item -ItemType directory -Path C:\Yourfolder -ErrorAction SilentlyContinue
+#Copy the file to folder
+Copy-Item $PSScriptRoot\Your_Lockscreen_v1.0.jpg C:\Yourfolder
+Copy-Item $PSScriptRoot\Your_Wallpaper_v1.0.jpg C:\Yourfolder
+#Disable Lockscreen to blur
+New-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" -ErrorAction SilentlyContinue
+New-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" -Name DisableAcrylicBackgroundOnLogon -Value "1" -PropertyType DWord -Force -ErrorAction SilentlyContinue
+#Set Lockscreen location 
+New-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Personalization" -ErrorAction SilentlyContinue
+New-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Personalization" -Name LockScreenImage -Value "C:\Yourfolder\Your_Lockscreen_v1.0.jpg" -PropertyType String -Force -ErrorAction SilentlyContinue
---
+++ Theme_User.ps1
@@ -0,0 +1,6 @@
+﻿#Set Custom Wallpaper in use
+New-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Control Panel\Desktop" -Name WallPaper -Value " C:\Yourfolder\Your_Wallpaper_v1.0.jpg" -PropertyType String -Force -ErrorAction SilentlyContinue
+#Set Taskbar color to match wallpaper (v1.5 v2.8, v4.0)
+New-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name ColorPrevalence -Value "1" -PropertyType DWord -Force
+#Automatically pick an color from wallpaper to windows menu (v1.7, v2.1 v2.8)
+New-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Control Panel\Desktop" -Name AutoColorization -Value "1" -PropertyType DWord -Force
