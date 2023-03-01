bec79fce804c5ee04e27669368ef61dc77f85c01
Pavel Mirochnitchenko <43411053+yannara@users.noreply.github.com>
2023-03-01 11:22:59 +0200

    Add files via upload

---
+++ RenameComputer.ps1
@@ -0,0 +1,11 @@
+﻿#Rename hostname based on prefix set and serial
+$Prefix = 'LB'
+$Serial = Get-WMIObject -Class "Win32_BIOS" | Select -Expand SerialNumber
+$NewComputername = $Prefix + $Serial
+Write-Output $NewComputername
+Rename-Computer -NewName $NewComputername -Force
+#Create Detection method
+New-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\LABS" -ErrorAction SilentlyContinue
+New-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\LABS\RenameComputer" -ErrorAction SilentlyContinue
+#Create registry value under recently created key
+New-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\LABS\RenameComputer" -Name Version -Value "1.0" -PropertyType String -Force -ErrorAction SilentlyContinue 
