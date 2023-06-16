1a5561faf1446d2dc2d3a8507277c83881f52a1c
Pavel Mirochnitchenko <43411053+yannara@users.noreply.github.com>
2023-06-16 13:24:14 +0300

    Add files via upload

---
+++ Install_vlc_winget_v1.4.ps1
@@ -0,0 +1,13 @@
+﻿#Start logging
+Start-Transcript -Path "C:\Windows\Debug\VLC-Player_1.4_Winget.txt"
+#Get Winget.exe from subfolder and execute silent installation
+Start-Process -FilePath "$PSScriptRoot\Winget\Winget.exe" -Wait -ArgumentList 'install VideoLAN.VLC --silent --accept-package-agreements --accept-source-agreements'
+#Waiting 30 seconds
+Start-Sleep -Seconds 30
+#Creating user default folder and copying config file there.
+New-Item -ItemType directory -Path C:\users\Default\Appdata\Roaming\vlc -Force
+Copy-Item -Path 'vlcrc' -Destination 'C:\users\Default\Appdata\Roaming\vlc' -Force
+#Creating detection method
+New-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\LABS\VLC" -ErrorAction SilentlyContinue
+New-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\LABS\VLC" -Name Version -Value "1.4" -PropertyType String -Force -ErrorAction SilentlyContinue
+Stop-Transcript
