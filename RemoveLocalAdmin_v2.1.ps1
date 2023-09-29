8168b87696999ea9db5bcb8a2b9c19d9b1a03e7a
Pavel Mirochnitchenko <43411053+yannara@users.noreply.github.com>
2023-09-29 15:19:26 +0300

    Delete RemoveLocalAdmin_v2.1.ps1

--- RemoveLocalAdmin_v2.1.ps1
+++
@@ -1,6 +0,0 @@
-﻿#Start logging
-Start-Transcript -Path "C:\Windows\Debug\RemoveAdmin_2.1.txt"
-#Remove current user from local admin group
-Remove-LocalGroupMember -SID "S-1-5-32-544" -Member "AzureAD\$env:USERNAME"
-#Stop logging
-Stop-Transcript
