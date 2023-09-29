aef71ead1354380cfc38ee50b33b0640d2d9703d
Pavel Mirochnitchenko <43411053+yannara@users.noreply.github.com>
2023-09-29 14:43:00 +0300

    Add files via upload

---
+++ RemoveLocalAdmin_v2.1.ps1
@@ -0,0 +1,6 @@
+﻿#Start logging
+Start-Transcript -Path "C:\Windows\Debug\RemoveAdmin_2.1.txt"
+#Remove current user from local admin group
+Remove-LocalGroupMember -SID "S-1-5-32-544" -Member "AzureAD\$env:USERNAME"
+#Stop logging
+Stop-Transcript
