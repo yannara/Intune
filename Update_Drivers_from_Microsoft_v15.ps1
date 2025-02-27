8bca450b4ba4de5a99e6819ffe622ebddb87be0a
Pavel Mirochnitchenko <43411053+yannara@users.noreply.github.com>
2025-02-27 13:43:29 +0200

    Add files via upload
    Changed log location to be able to pull logs with Device Diagnostics

---
+++ Update_Drivers_from_Microsoft_v15.ps1
@@ -0,0 +1,29 @@
+﻿#Start logging
+Start-Transcript -Path "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\LABS_Drivers_Update_v15.log"
+#Install prerequisite
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.021 -Force
+Install-Module -Name PSWindowsUpdate -Force
+Import-Module -Name PSWindowsUpdate -Force
+#Register to MS Update Service
+Add-WUServiceManager -ServiceID "7971f918-a847-4430-9279-4a52d1efe18d" -Confirm:$false
+#Download and install drivers. Multiple times because command might break between driver installs
+Install-WindowsUpdate -Install -AcceptAll -UpdateType Driver -MicrosoftUpdate -ForceDownload -ForceInstall -IgnoreReboot -RecurseCycle 4 | Out-File "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\LABS_Drivers_Install_from_Microsoft_Catalog_part1_$(Get-date -Format dd-MMM-yyyy-hh.mm.ss).log" -Force -ErrorAction SilentlyContinue
+Start-Sleep -s 30
+Install-WindowsUpdate -Install -AcceptAll -UpdateType Driver -MicrosoftUpdate -ForceDownload -ForceInstall -IgnoreReboot -RecurseCycle 4 | Out-File "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\LABS_Drivers_Install_from_Microsoft_Catalog_part2_$(Get-date -Format dd-MMM-yyyy-hh.mm.ss).log" -Force -ErrorAction SilentlyContinue
+Start-Sleep -s 30
+Install-WindowsUpdate -Install -AcceptAll -UpdateType Driver -MicrosoftUpdate -ForceDownload -ForceInstall -IgnoreReboot -RecurseCycle 4 | Out-File "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\LABS_Drivers_Install_from_Microsoft_Catalog_part3_$(Get-date -Format dd-MMM-yyyy-hh.mm.ss).log" -Force -ErrorAction SilentlyContinue
+Start-Sleep -s 30
+Install-WindowsUpdate -Install -AcceptAll -UpdateType Driver -MicrosoftUpdate -ForceDownload -ForceInstall -IgnoreReboot -RecurseCycle 4 | Out-File "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\LABS_Drivers_Install_from_Microsoft_Catalog_part4_$(Get-date -Format dd-MMM-yyyy-hh.mm.ss).log" -Force -ErrorAction SilentlyContinue
+Start-Sleep -s 30
+Install-WindowsUpdate -Install -AcceptAll -UpdateType Driver -MicrosoftUpdate -ForceDownload -ForceInstall -IgnoreReboot -RecurseCycle 4 | Out-File "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\LABS_Drivers_Install_from_Microsoft_Catalog_part5_$(Get-date -Format dd-MMM-yyyy-hh.mm.ss).log" -Force -ErrorAction SilentlyContinue
+Start-Sleep -s 30
+Install-WindowsUpdate -Install -AcceptAll -UpdateType Driver -MicrosoftUpdate -ForceDownload -ForceInstall -IgnoreReboot -RecurseCycle 4 | Out-File "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\LABS_Drivers_Install_from_Microsoft_Catalog_part6_$(Get-date -Format dd-MMM-yyyy-hh.mm.ss).log" -Force -ErrorAction SilentlyContinue
+Start-Sleep -s 30
+Install-WindowsUpdate -Install -AcceptAll -UpdateType Driver -MicrosoftUpdate -ForceDownload -ForceInstall -IgnoreReboot -RecurseCycle 4 | Out-File "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\LABS_Drivers_Install_from_Microsoft_Catalog_part7_$(Get-date -Format dd-MMM-yyyy-hh.mm.ss).log" -Force -ErrorAction SilentlyContinue
+Start-Sleep -s 30
+Install-WindowsUpdate -Install -AcceptAll -UpdateType Driver -MicrosoftUpdate -ForceDownload -ForceInstall -IgnoreReboot -RecurseCycle 4 | Out-File "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\LABS_Drivers_Install_from_Microsoft_Catalog_part8_$(Get-date -Format dd-MMM-yyyy-hh.mm.ss).log" -Force -ErrorAction SilentlyContinue
+Start-Sleep -s 30
+#Create detection method
+Add-Content "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\LABS_Drivers_Update_v15.log" "Drivers update completed" -ErrorAction SilentlyContinue
+#Stop logging
+Stop-Transcript
