ca4c2f9e530aa62e22f31a010115ecf06401b7d6
Pavel Mirochnitchenko <43411053+yannara@users.noreply.github.com>
2025-08-11 09:39:45 +0300

    Add files via upload

---
+++ Uninstall_driver_updates.ps1
@@ -0,0 +1,6 @@
+﻿#Start logging
+Start-Transcript -Path "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\LABS_Uninstall_Drivers_Update.log"
+#Uninstall this app by deleting folder with logs
+Remove-Item -Recurse -Force -Path "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\LABS_Drivers_Update_v15.log"
+#Stop logging
+Stop-Transcript
