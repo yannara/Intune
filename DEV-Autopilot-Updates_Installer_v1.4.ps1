2a6b4e8441f2e3ccfc0170f7d0f9bdd0f23c8147
Pavel Mirochnitchenko <43411053+yannara@users.noreply.github.com>
2026-06-02 11:18:37 +0300

    Add files via upload

---
+++ DEV-Autopilot-Updates_Installer_v1.4.ps1
@@ -0,0 +1,28 @@
+﻿#Start logging
+Start-Transcript -Path "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\Device_Autopilot_Updates_v1.4.log"
+#Install prerequisite
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.021 -Force
+Install-Module -Name PSWindowsUpdate -Force
+Import-Module -Name PSWindowsUpdate -Force
+#Register to MS Update Service
+Add-WUServiceManager -ServiceID "7971f918-a847-4430-9279-4a52d1efe18d" -Confirm:$false
+#Download and install drivers. Multiple times because command might break between driver installs
+Install-WindowsUpdate -Install -AcceptAll -UpdateType Software -MicrosoftUpdate -NotCategory 'Definition Updates' -Download -IgnoreReboot | Out-File "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\Device_Autopilot_Updates_Install_part1_$(Get-date -Format dd-MMM-yyyy-hh.mm.ss).log" -Force -ErrorAction SilentlyContinue
+Start-Sleep -s 30
+Install-WindowsUpdate -Install -AcceptAll -UpdateType Software -MicrosoftUpdate -NotCategory 'Definition Updates' -Download -IgnoreReboot | Out-File "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\Device_Autopilot_Updates_Install_part2_$(Get-date -Format dd-MMM-yyyy-hh.mm.ss).log" -Force -ErrorAction SilentlyContinue
+Start-Sleep -s 30
+Install-WindowsUpdate -Install -AcceptAll -UpdateType Software -MicrosoftUpdate -NotCategory 'Definition Updates' -Download -IgnoreReboot | Out-File "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\Device_Autopilot_Updates_Install_part3_$(Get-date -Format dd-MMM-yyyy-hh.mm.ss).log" -Force -ErrorAction SilentlyContinue
+Start-Sleep -s 30
+Install-WindowsUpdate -Install -AcceptAll -UpdateType Software -MicrosoftUpdate -NotCategory 'Definition Updates' -Download -IgnoreReboot | Out-File "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\Device_Autopilot_Updates_Install_part4_$(Get-date -Format dd-MMM-yyyy-hh.mm.ss).log" -Force -ErrorAction SilentlyContinue
+Start-Sleep -s 30
+Install-WindowsUpdate -Install -AcceptAll -UpdateType Software -MicrosoftUpdate -NotCategory 'Definition Updates' -Download -IgnoreReboot | Out-File "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\Device_Autopilot_Updates_Install_part5_$(Get-date -Format dd-MMM-yyyy-hh.mm.ss).log" -Force -ErrorAction SilentlyContinue
+Start-Sleep -s 30
+Install-WindowsUpdate -Install -AcceptAll -UpdateType Software -MicrosoftUpdate -NotCategory 'Definition Updates' -Download -IgnoreReboot | Out-File "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\Device_Autopilot_Updates_Install_part6_$(Get-date -Format dd-MMM-yyyy-hh.mm.ss).log" -Force -ErrorAction SilentlyContinue
+Start-Sleep -s 30
+Install-WindowsUpdate -Install -AcceptAll -UpdateType Software -MicrosoftUpdate -NotCategory 'Definition Updates' -Download -IgnoreReboot | Out-File "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\Device_Autopilot_Updates_Install_part7_$(Get-date -Format dd-MMM-yyyy-hh.mm.ss).log" -Force -ErrorAction SilentlyContinue
+Start-Sleep -s 30
+Install-WindowsUpdate -Install -AcceptAll -UpdateType Software -MicrosoftUpdate -NotCategory 'Definition Updates' -Download -IgnoreReboot | Out-File "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\Device_Autopilot_Updates_Install_part8_$(Get-date -Format dd-MMM-yyyy-hh.mm.ss).log" -Force -ErrorAction SilentlyContinue
+Start-Sleep -s 30
+#Stop logging
+Stop-Transcript
+New-Item "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\Device_Autopilot_Updates_v1.4_detect.log" -ItemType File -Value "LABS Update Installer during Autopilot"
