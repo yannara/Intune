325c0cc95cfdf65bad4cad3c6cd20af87777d6d3
Pavel Mirochnitchenko <43411053+yannara@users.noreply.github.com>
2026-09-18 13:55:51 +0300

    Delete Device_Configuration.ps1

--- Device_Configuration.ps1
+++
@@ -1,3 +0,0 @@
-﻿#Disable Lockscreen to blur
-New-Item -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" -ErrorAction SilentlyContinue
-New-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" -Name DisableAcrylicBackgroundOnLogon -Value "1" -PropertyType DWord -Force -ErrorAction SilentlyContinue
